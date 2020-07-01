85fa19d5db492db49af172ef02c9ad80561b4c9c
Nicolas Vong <nicolas.vong@energies-demain.com>
2020-07-01 22:35:25 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Footylights</h1>
+    <Highlights/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Highlights from './components/Highlights.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Highlights
   }
 }
 </script>
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
 </style>
